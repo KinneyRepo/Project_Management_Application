9877fddbfba3c200aab805a732b2194e11ec11bb
Joe Kinney <joekinney53@gmail.com>
2023-04-06 18:30:03 -0500

    added some css to login page

--- login.php
+++ login.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f0f4f8;
+         body {
+            font-family: 'Poppins', sans-serif;
+            background-color: #345d84;
             margin: 0;
             padding: 0;
             display: flex;
@@ -16,55 +17,97 @@
             height: 100vh;
         }
 
+        #signUpBtn {
+            text-align: center;
+        }
+
+        #clientLoginLink {
+            text-align: end;
+        }
+
+        a {
+            color: #345d84;
+        }
+
         .container {
+            position: relative;
             background-color: #ffffff;
-            padding: 20px;
+            padding: 30px 30px 50px;
             border-radius: 5px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-            width: 300px;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            width: 400px;
         }
 
         h1 {
             text-align: center;
-            margin-bottom: 10px;
+            margin-bottom: 30px;
+            font-weight: 400;
+            color: #345d84;
         }
 
         p {
             text-align: center;
-            margin-bottom: 20px;
+            margin-bottom: 30px;
+            font-weight: 300;
+            color: #345d84;
         }
 
         label {
             display: block;
             margin-bottom: 5px;
+            font-weight: 300;
+            color: #345d84;
         }
 
         input {
             width: 100%;
-            padding: 5px;
+            padding: 10px;
             margin-bottom: 20px;
+            border: 1px solid #c4d4e2;
+            border-radius: 5px;
+            font-size: 14px;
+            background-color: #ffffff;
         }
 
         input[type="submit"] {
-            width: auto;
-            background-color: #007bff;
+            width: 100%;
+            background-color: #345d84;
             color: white;
             border: none;
             cursor: pointer;
-            padding: 8px 16px;
+            padding: 12px 0;
             margin-top: 10px;
-            border-radius: 3px;
+            border-radius: 5px;
+            font-size: 16px;
+            font-weight: 400;
         }
 
         input[type="submit"]:hover {
-            background-color: #0056b3;
+            background-color: #2d4a6b;
+        }
+
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(255, 255, 255, 0.6);
+            display: flex;
+            justify-content: center;
+            align-items: center;
         }
     </style>
 </head>
 <body>
 <div class="container">
+    <div id = "clientLoginLink">
+    <a href="">Client login</a>
+    </div>
+    <div>
     <h1>Login</h1>
     <p>Please enter your username and password</p>
+    </div>
     <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         require 'connect.php';
@@ -114,7 +157,12 @@
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required>
         <br>
+        <div id = "loginBtn">
         <input type="submit" value="Login">
+        </div>
+        <div id = "signUpBtn">
+        <a href="">Sign Up</a>
+        </div>
     </form>
 </div>
 </body>
